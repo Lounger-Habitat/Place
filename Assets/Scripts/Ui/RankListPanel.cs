32a775101fc3a1c608ca42b716b8e0a5430b26a5
ZJDllll <gt6235@163.com>
2023-12-25 20:54:42 +0800

    feat: 添加UI脚本控制，rank列表与team列表

---
+++ Assets/Scripts/Ui/RankListPanel.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankListPanel : MonoBehaviour
+{
+    private Transform rankItem;
+    private List<Transform> useList = new List<Transform>();
+    private List<Transform> poolList = new List<Transform>();
+    public void Init()
+    {
+        rankItem = transform.GetChild(0);
+        rankItem.gameObject.SetActive(false);
+    }
+
+    public void SetData(List<RankItem> rankItems)
+    {
+        //先删除
+        var count = useList.Count;
+        for (int i = 1; i <= count; i++)
+        {
+            Destroy(useList[count - i].gameObject);
+        }
+        useList.Clear();
+        //再生成
+        foreach (var item in rankItems)
+        {
+            var rankTransform = Instantiate(rankItem, transform);
+            rankTransform.gameObject.SetActive(true);
+            rankTransform.Find("Name").GetComponent<TMP_Text>().text = item.userName;
+            rankTransform.Find("Data").GetComponent<TMP_Text>().text = item.rankData;
+            if (item.iconTexture != null)
+            {
+                Sprite sp = Sprite.Create(item.iconTexture, new Rect(0, 0, item.iconTexture.width, item.iconTexture.height), new Vector2(.5f, .5f));
+                rankTransform.Find("Icon").GetComponent<Image>().sprite = sp;
+            }
+            useList.Add(rankTransform);
+        }
+        //刷新UI布局
+        LayoutRebuilder.ForceRebuildLayoutImmediate(transform as RectTransform);
+    }
+}
+
+public struct RankItem
+{
+    /// <summary>
+    /// 玩家名称
+    /// </summary>
+    public string userName;
+    /// <summary>
+    /// 分数
+    /// </summary>
+    public string rankData;
+    /// <summary>
+    /// 前方图标
+    /// </summary>
+    public Texture2D iconTexture;
+}
---
+++ Assets/Scripts/Ui/TeamListPanel.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeamListPanel : MonoBehaviour
+{
+    private Transform teamItem;
+    private List<Transform> useList = new List<Transform>();
+    private List<Transform> poolList = new List<Transform>();
+    public void Init(){
+        teamItem  = transform.GetChild(0);
+        teamItem.gameObject.SetActive(false);
+    }
+
+    public void SetData(List<TeamItem> rankItems){
+        //先删除
+        var count = useList.Count;
+        for (int i = 1; i <= count; i++)
+        {
+           Destroy(useList[count-i].gameObject);
+        }
+        useList.Clear();
+        //再生成
+        foreach (var item in rankItems)
+        {
+            var teamTransform = Instantiate(teamItem,transform);
+            teamTransform.gameObject.SetActive(true);
+            teamTransform.Find("TeamName").GetComponent<TMP_Text>().text = item.teamName;
+            teamTransform.Find("Data").GetComponent<TMP_Text>().text = item.teamNumber;
+            if (item.iconTexture!=null)
+            {
+                Sprite sp = Sprite.Create(item.iconTexture,new Rect(0,0,item.iconTexture.width,item.iconTexture.height),new Vector2(.5f,.5f));
+                teamTransform.Find("Icon").GetComponent<Image>().sprite = sp;
+            }
+            useList.Add(teamTransform);
+        }
+        //刷新UI
+        LayoutRebuilder.ForceRebuildLayoutImmediate(transform as RectTransform);
+    }
+}
+
+public struct TeamItem{
+    /// <summary>
+    /// 团队名称
+    /// </summary>
+    public string teamName;
+    /// <summary>
+    /// 团队人数，目前这么写，可能是别的
+    /// </summary>
+    public string teamNumber;
+    /// <summary>
+    /// 团队图标
+    /// </summary>
+    public Texture2D iconTexture;
+}
