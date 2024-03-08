95c89cedfbe1b94871691451b52a79d440f4e0c9
GT888 <gt8888888@outlook.com>
2024-03-08 17:36:46 +0800

    feat: 添加GUI 简单的UI修改

---
+++ Assets/Scripts/Ui/Tips/MessageTips.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageTips : TipsBase
+{
+    public override void SetData(TipsItem tips)
+    {
+        
+    }
+
+    public override void MoveTipsPanel(bool isOn = true)
+    {
+        
+    }
+}
