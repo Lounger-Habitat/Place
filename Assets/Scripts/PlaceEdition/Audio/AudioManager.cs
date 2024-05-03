93f6b4eb7ca19a3c68da49d6d3165882a68f9bd4
GT888 <gt8888888@outlook.com>
2024-05-03 09:56:59 +0800

    feat: 添加音乐管理，设置页面增加声音控制

---
+++ Assets/Scripts/PlaceEdition/Audio/AudioManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    
+    public AudioSource audioSource;
+
+    private int audioIndex = 0;
+    
+    public void Init()
+    {
+        PlaySound(0);
+    }
+
+    public void PlaySound(int index)
+    {
+        audioIndex = index;
+        audioSource.clip = audioClips[audioIndex];
+        audioSource.time = 0;
+        audioSource.Play();
+        //audioSource.PlayOneShot(audioClips[audioIndex]);
+    }
+
+    public void Btn_click()
+    {
+        if (audioIndex.Equals(0))
+        {
+            PlaySound(1);
+        }
+        else
+        {
+            PlaySound(0);
+        }
+    }
+
+    private bool noAudio = false;
+    public void Slider_Audio(float value)
+    {
+        audioSource.volume = value;
+        if (value>0.01f)
+        {
+            audioSource.mute =false;
+            muteImage.sprite = audioSource.mute ? muteSprite[0] : muteSprite[1];
+        }
+    }
+
+
+    public Image muteImage;
+    public Sprite[] muteSprite;
+    public void Mute_Click()
+    {
+        audioSource.mute = !audioSource.mute;
+        muteImage.sprite = audioSource.mute ? muteSprite[0] : muteSprite[1];
+    }
+}
