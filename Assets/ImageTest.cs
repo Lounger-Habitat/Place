596e798454782f8df93533a0f3359b4d4115e505
GCYYfun <734536637@qq.com>
2024-06-07 15:07:37 +0800

    feat: add opencv package

---
+++ Assets/ImageTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCVForUnity.ImgprocModule;
+
+public class ImageTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Imgproc.cvtColor
+        // Imgproc.Canny
+        // Imgproc.findContours
+        // Imgproc.drawContours
+        // Imgproc.rectangle
+        // Imgproc.boundingRect
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
